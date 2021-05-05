3ebbd44d985476bf434a23629dbb3479f5bfddd1
cmuriuki <c.muriukin@gmail.com>
2021-05-05 05:37:57 -0400

    Polling test

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -1,3 +1,9 @@
+ @Test
+  public void greetShouldIncludeGreetingMessage() {
+    String someone = "World, Thanks you making me Happy";
+
+    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
+  }
 package com.example;
 
 import org.junit.Before;
@@ -37,4 +43,12 @@ public class TestGreeter {
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
+
+ @Test
+  public void greetShouldIncludeGreetingMessage() {
+    String someone = "Hello World, I am comming to get you!!!!!!";
+
+    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
+  }
+
 }
