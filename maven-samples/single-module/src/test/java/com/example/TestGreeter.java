974221cbd54b477e122bf1560aa3ccb6da2660e5
cmuriuki <c.muriukin@gmail.com>
2021-05-05 07:47:35 -0400

    Happy

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -33,7 +33,7 @@ public class TestGreeter {
 
     @Test
   public void greetShouldIncludeGreetingMessage() {
-    String someone = "World, Thanks you making me Happy mmm";
+    String someone = "World, Thanks you making me Happy, SMile";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
